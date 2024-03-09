6e10336b1eb607bfb503ac0a87538041923dcadf
MeirzhanHg <mikimikomiko03@gmail.com>
2024-03-09 21:14:34 +0500

    aaa

--- js/script.js
+++ js/script.js
@@ -5,106 +5,106 @@ function sliderMore(
   nextSlide,
   sliders
 ) {
-  let position = 0;
-  let slidesToShow = 3;
+  let position = 0
+  let slidesToShow = 3
   if (window.innerWidth <= 1100) {
-    slidesToShow = 2;
+    slidesToShow = 2
   }
   if (window.innerWidth <= 600) {
-    slidesToShow = 1;
+    slidesToShow = 1
   }
 
-  const slidesToScroll = 1;
-  const container = document.querySelector(containerSlider);
-  const track = document.querySelector(trackSlider);
+  const slidesToScroll = 1
+  const container = document.querySelector(containerSlider)
+  const track = document.querySelector(trackSlider)
 
-  const btnPrev = document.querySelector(prevSlide);
-  const btnNext = document.querySelector(nextSlide);
-  const items = document.querySelectorAll(sliders);
+  // const btnPrev = document.querySelector(prevSlide);
+  // const btnNext = document.querySelector(nextSlide);
+  const items = document.querySelectorAll(sliders)
 
-  const itemsCount = items.length;
-  const itemWidth = container.clientWidth / slidesToShow;
-  const movePosition = slidesToScroll * itemWidth;
+  const itemsCount = items.length
+  const itemWidth = container.clientWidth / slidesToShow
+  const movePosition = slidesToScroll * itemWidth
 
-  let touchStartX = 0;
-  let touchEndX = 0;
+  let touchStartX = 0
+  let touchEndX = 0
 
   track.addEventListener("touchstart", (event) => {
-    touchStartX = event.touches[0].clientX;
-  });
+    touchStartX = event.touches[0].clientX
+  })
 
   track.addEventListener("touchend", (event) => {
-    touchEndX = event.changedTouches[0].clientX;
-    handleSwipe();
-  });
+    touchEndX = event.changedTouches[0].clientX
+    handleSwipe()
+  })
 
   function handleSwipe() {
-    const swipeThreshold = Math.abs(touchStartX - touchEndX);
+    const swipeThreshold = Math.abs(touchStartX - touchEndX)
 
     if (swipeThreshold < 50) {
       // Consider it as a tap or negligible swipe
-      return;
+      return
     }
 
     if (touchStartX > touchEndX) {
       // Swipe left, move to next slide
-      moveSlide("next");
+      moveSlide("next")
     } else {
       // Swipe right, move to previous slide
-      moveSlide("prev");
+      moveSlide("prev")
     }
   }
 
   function moveSlide(direction) {
     if (direction === "next") {
-      position -= movePosition;
-      position = Math.max(position, -(itemsCount - slidesToShow) * itemWidth);
+      position -= movePosition
+      position = Math.max(position, -(itemsCount - slidesToShow) * itemWidth)
     } else {
-      position += movePosition;
-      position = Math.min(position, 0);
+      position += movePosition
+      position = Math.min(position, 0)
     }
 
-    setPosition();
-    checkBtns();
+    setPosition()
+    // checkBtns();
   }
 
   // ............................
 
   items.forEach((item) => {
-    item.style.minWidth = `${itemWidth - 15}px`;
-  });
+    item.style.minWidth = `${itemWidth - 15}px`
+  })
 
-  btnNext.addEventListener("click", () => {
-    const itemsLeft =
-      itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+  // btnNext.addEventListener("click", () => {
+  //   const itemsLeft =
+  //     itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
 
-    position -=
-      itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+  //   position -=
+  //     itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
 
-    setPosition();
-    checkBtns();
-  });
+  //   setPosition();
+  //   checkBtns();
+  // });
 
-  btnPrev.addEventListener("click", () => {
-    const itemsLeft = Math.abs(position) / itemWidth;
+  // btnPrev.addEventListener("click", () => {
+  //   const itemsLeft = Math.abs(position) / itemWidth;
 
-    position +=
-      itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+  //   position +=
+  //     itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
 
-    setPosition();
-    checkBtns();
-  });
+  //   setPosition();
+  //   checkBtns();
+  // });
 
   const setPosition = () => {
-    track.style.transform = `translateX(${position}px)`;
-  };
+    track.style.transform = `translateX(${position}px)`
+  }
 
   const checkBtns = () => {
-    btnPrev.disabled = position === 0;
-    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
-  };
+    btnPrev.disabled = position === 0
+    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth
+  }
 
-  checkBtns();
+  // checkBtns();
 }
 
 sliderMore(
@@ -113,4 +113,53 @@ sliderMore(
   ".prev-lifestyle",
   ".next-lifestyle",
   ".box-item"
-);
+)
+
+
+
+
+// Accordion
+
+const accordion = (triggersSelector) => {
+  const btns = document.querySelectorAll(triggersSelector)
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', function () {
+
+      btns.forEach(btn => {
+        if (!this.classList.contains('active-style')) {
+
+          btn.classList.remove('active-style')
+
+          btn.nextElementSibling.classList.remove('active-content')
+
+          btn.nextElementSibling.style.maxHeight = 0 + 'px'
+        }
+
+      })
+
+      this.classList.toggle('active-style')
+      this.nextElementSibling.classList.toggle('active-content')
+
+      if (this.classList.contains('active-style')) {
+        this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + 'px'
+      } else {
+        this.nextElementSibling.style.maxHeight = '0px'
+      }
+    })
+
+    btns.forEach(btn => {
+      if (btn.classList.contains('active-style')) {
+        btn.nextElementSibling.style.maxHeight = btn.nextElementSibling.scrollHeight + 80 + 'px'
+      } else {
+        btn.nextElementSibling.style.maxHeight = '0px'
+      }
+    })
+
+
+  })
+
+}
+
+accordion('.faq-heading')
+
